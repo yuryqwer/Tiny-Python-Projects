5a77b2c93d1694d908062f2fe11ba00b7f575adf
yuryqwer <yuryqwer@outlook.com>
2020-09-11 16:26:05 +0800

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,12 +1,14 @@
-def run_cmd(path, cond, timeout=None):
+def run_cmd_until_cond(path, cond, timeout=None):
     """
+    运行命令行程序，当输出行满足cond条件时运行结束
+    
     用子进程（这边的子进程不是python）运行程序并逐行判断输出
     如果输出满足某种条件则结束子进程
     注意bat文件如果有中文的话要用ANSI编码
     尽量设置超时时间，如果不设的话，当运行的bat中有pause
     且碰到pause时还没遇到符合条件的行就会一直卡死
 
-    >>> run_cmd(r'cd .. && dir', lambda text: False, 10)
+    >>> run_cmd_until_cond(r'cd .. && dir', lambda text: False, 10)
     """
     import threading
     import subprocess
